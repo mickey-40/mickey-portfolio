be9283b653b34a0501f94ab81cbc7b7dec5b477c
mickey-40 <mickeyarnold53@yahoo.com>
2023-03-17 09:40:27 -0500

    added .gitignore

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css'
+import {Route, Router, Routes} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
