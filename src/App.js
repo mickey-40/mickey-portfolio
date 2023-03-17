b3413ade94ec0eec5da2f8b7ca3c9586d4e0105b
mickey-40 <mickeyarnold53@yahoo.com>
2023-03-17 15:44:45 -0500

    complete navbar with basic css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css'
 import { Routes, Route} from 'react-router-dom'
-import Home from './Pages/Home';
-import Projects from './Pages/Projects';
-import Experience from './Pages/Experience';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Experience from './pages/Experience';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      
+        <Navbar />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/projects' element={<Projects />} />
---
+++ src/components/Navbar.js
@@ -0,0 +1,35 @@
+import React, {useState, useEffect} from 'react'
+import { Link, useLocation } from 'react-router-dom'
+import '../styles/Navbar.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse, faFolder, faAtom, faBars } from '@fortawesome/free-solid-svg-icons'
+
+function Navbar() {
+  const[expandNavbar, setExpandNavbar] = useState(false);
+
+  const location = useLocation()
+
+  useEffect(()=>{
+    setExpandNavbar(false)
+  }, [location])
+
+  return (
+    <div className='navbar' id={expandNavbar ? "open" : "close"}>
+      <div className='toggleButton'>
+        <button onClick={()=>{setExpandNavbar((prev)=>!prev)}}>
+          <FontAwesomeIcon icon={faBars} /></button>
+      </div>
+      <div className='links'>
+        <div></div>
+        <FontAwesomeIcon icon={faHouse} />
+        <Link to="/">Home</Link>
+        <FontAwesomeIcon icon={faFolder} />
+        <Link to="/projects">Projects</Link>
+        <FontAwesomeIcon icon={faAtom} />
+        <Link to="/experience">Experience</Link>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
