f141f17b52bff1e91790aeb33b6ac73a85977af6
mickey-40 <mickeyarnold53@yahoo.com>
2023-03-18 21:44:56 -0500

    put project images and names on the projects.js file

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import ProjectItem from '../components/ProjectItem'
+import { projectList } from '../helpers/ProjectList'
+import '../styles/Project.css'
 
 function Projects() {
   return (
-    <div>Hello from Projects</div>
+    <div className='projects'>
+      <h1> My Personal Projects</h1>
+      <div className='projectList'>
+        {projectList.map((project)=>{
+          return <ProjectItem name={project.name} image={project.image}/>
+        })}
+      </div>
+    </div>
   )
 }
 
---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,24 @@
+import Proj1 from '../assets/Proj1.png'
+import Proj2 from '../assets/Proj2.jpg'
+import Proj3 from '../assets/Proj3.jpg'
+import Proj4 from '../assets/Proj4.jpg'
+
+export const projectList = [
+  {
+    name: "Middle Math Quiz", 
+    image: Proj1
+  },
+  {
+    name: "Job Tracker", 
+    image: Proj2
+  },
+  {
+    name: "Journey", 
+    image: Proj3
+  },
+  {
+    name: "Burgers and Beers", 
+    image: Proj4
+  }
+
+]
